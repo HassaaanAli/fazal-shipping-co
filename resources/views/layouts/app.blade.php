b57dc437da8c854bd464d105ceb892c082bc035c
HassaaanAli <144128640+HassaaanAli@users.noreply.github.com>
2024-02-08 23:59:49 +0500

    theme customization

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,87 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="zxx" class="js">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <!-- <base href="../"> -->
+    <meta charset="utf-8">
+    <meta name="author" content="Softnio">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Tasks">
+    <!-- Fav Icon  -->
+    <link rel="shortcut icon" href="{{asset('images/favicon.png')}}">
+    <!-- Page Title  -->
+    <title>Shipping</title>
+    <!-- StyleSheets  -->
+    <link rel="stylesheet" href="{{asset('assets/css/dashlite.css')}}">
+    <link id="skin-default" rel="stylesheet" href="{{asset('assets/css/theme.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/libs/bootstrap-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/libs/fontawesome-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/libs/themify-icons.css')}}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <style>
+        div.dataTables_wrapper div.dataTables_length select {
+            margin: 10px;
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+        }
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        .dataTables_filter {
+            float: right;
+        }
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+        div.dataTables_wrapper div.dataTables_filter input {
+            width: 190px;
+        }
+    </style>
+    <!-- Scripts -->
+    @stack("styles")
+	@yield("styles")
+</head>
+
+<body class="nk-body bg-lighter npc-general has-sidebar ">
+    <div class="nk-app-root">
+        <!-- main @s -->
+        <div class="nk-main ">
+            <!-- sidebar @s -->
+            @include('layouts.sidebar')
+            <!-- sidebar @e -->
+
+            <!-- wrap @s -->
+            <div class="nk-wrap ">
+                <!-- main header @s -->
+                @include('layouts.header')
+                <!-- main header @e -->
+
+                <!-- content @s -->
+                @yield('content')
+                <!-- content @e -->
+
+                <!-- footer @s -->
+                @include('layouts.footer')
+                <!-- footer @e -->
+            </div>
+            <!-- wrap @e -->
         </div>
-    </body>
+        <!-- main @e -->
+    </div>
+    <!-- app-root @e -->
+
+    <!-- JavaScript -->
+    <script src="{{asset('assets/js/bundle.js')}}"></script>
+    <script src="{{asset('assets/js/vendors/nioapp/nioapp.min.js')}}"></script>
+    <script src="{{asset('assets/js/scripts.js')}}"></script>
+    <script src="{{asset('assets/js/example-sweetalert.js')}}"></script>
+    <script src="{{asset('assets/js/example-toastr.js')}}"></script>
+    <script>
+		$(document).ready(function() {
+			// $('[data-toggle="tooltip"]').tooltip();
+			// $('[data-toggle="popover"]').popover();
+			// $('.delete-record').on("click", function(e) {
+			// 	e.preventDefault();
+			// });
+		});
+	</script>
+    @stack("scripts")
+
+</body>
+
 </html>
